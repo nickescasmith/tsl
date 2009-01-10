4d7aa044199c83b408cb01311f11faca0b749bee
John Burton <john.burton@jbmail.com>
2009-01-10 19:54:02 +0000

    Initial commit

---
+++ TradeSkillList.lua
@@ -0,0 +1,162 @@
+--
+-- (this version Hacked by John Burton (john.burton@jbmail.com) to 
+-- output the BBCOde I need
+--
+-- TradeSkillList.lua
+-- by Eric Shepherd
+-- 
+--
+-- Version 0.7.2
+--
+-- History:
+--
+-- 0.7.2: Fixed enchanting links to Thottbot to work correctly.
+-- 0.7.1: Updated for WoW 3.0.
+-- 0.7: Updated for WoW 2.3.  Also, Thottbot links now give tooltips on hover.
+-- 0.6.2: Make the scroll bar work.
+-- 0.6.1: Updated for WoW 2.2 compatibility.
+-- 0.6: Now shows links to each subsection at the top of the generated page
+-- 0.5.2: Fixed to show enchanting rank properly.  Also uses color to determine
+--        rarity, since gems don't have "rare/uncommon/epic" tags in them.
+-- 0.5.1: Fixed to let you close the window after opening it a second time
+-- 0.5: Initial release
+--
+
+function TradeSkillList_Close()
+  TradeSkillListFrame:Hide();
+  TradeSkillListFrame.Box:SetText("");
+end
+
+function StartBuildingSkillList(cmd)
+  local topText = "";
+  local text = "";
+  local tradeSkillName, currentLevel, maxLevel;
+  local numSkills;
+  local curSkillIndex;
+  local skillName, skillType;
+  local itemLink, itemString;
+  local subSpellName;
+  local itemId;
+  local i;
+  local doneFirstTopLink = 0;
+  local doingEnchants = 0;
+
+  DEFAULT_CHAT_FRAME:AddMessage(TRADESKILL_STARTING);
+  
+  -- First, get the name of the currently open trade skill window.
+  
+  tradeSkillName, currentLevel, maxLevel = GetTradeSkillLine();
+  
+  if tradeSkillName == TRADESKILL_UNKNOWN then
+    DEFAULT_CHAT_FRAME:AddMessage(TRADESKILL_NONE_OPEN);
+    return;
+  end
+  
+  
+  local player_name
+  player_name= UnitName("player");
+
+  itemList = 'Tradeskills for ' .. player_name .. "\r\n\r\n";
+
+  -- Now we iterate over the skill list
+  
+  numSkills = GetNumTradeSkills();      -- Includes headers
+  
+  for curSkillIndex=1, numSkills do
+    local found;
+    local i;
+    local numLines;
+    local desc;
+    local typeChar;
+    local itemStats;
+        
+    skillName, skillType, _, _ = GetTradeSkillInfo(curSkillIndex);
+    
+    if (skillType == TRADESKILL_HEADER_TYPE) then
+      local tweakedName = skillName.gsub(skillName, " ", "&nbsp;");
+      doneFirstTopLink = 1;
+      if (skillName == TRADESKILL_ENCHANT) then
+        doingEnchants = 1;
+      else
+        doingEnchants = 0;
+      end
+      itemList = itemList .. '\r\n[b]' .. skillName .. '[/b]\r\n';
+    else
+      itemLink = GetTradeSkillItemLink(curSkillIndex);
+      found, _, color, itemString = string.find(itemLink, "^|c(%x+)|H(.+)|h%[.+%]");
+      found, itemId, _, _, _, _, _, _, _, _ = strsplit(":", itemString);
+      
+      TradeSkillListScanTooltip:ClearLines();
+      TradeSkillListScanTooltip:SetTradeSkillItem(curSkillIndex);
+    
+      numLines = TradeSkillListScanTooltip:NumLines();
+
+      itemList = itemList .. '[item]'.. skillName .. '[/item]\r\n';
+      
+    end
+  end
+
+  
+  DEFAULT_CHAT_FRAME:AddMessage(TRADESKILL_DONE);
+  
+  -- Show the trade skill list window
+  
+  local skillFrame = nil;
+  
+  skillFrame = TradeSkillListFrame;
+  if skillFrame == nil then
+    skillFrame = CreateFrame("Frame", "TradeSkillListFrame", UIParent);
+  end
+  
+  skillFrame:Hide();
+  skillFrame:SetPoint("CENTER", "UIParent", "CENTER");
+  skillFrame:SetFrameStrata("DIALOG");
+  skillFrame:SetHeight(600);
+  skillFrame:SetWidth(800);
+  skillFrame:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+		edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+		tile = true, tileSize = 32, edgeSize = 32,
+		insets = { left = 9, right = 9, top = 9, bottom = 9 }
+	});
+	skillFrame:SetBackdropColor(0, 0, 0, 0.8);
+	
+  -- Add the Close button
+  
+  skillFrame.DoneButton = CreateFrame("Button", "TradeSkillListViewCloseButton", skillFrame, "OptionsButtonTemplate");
+  skillFrame.DoneButton:SetText(TRADESKILL_CLOSE);
+  skillFrame.DoneButton:SetPoint("BOTTOMRIGHT", skillFrame, "BOTTOMRIGHT", -10, 10);
+  skillFrame.DoneButton:SetScript("OnClick", TradeSkillList_Close);
+  
+  -- Create the scroll frame
+  
+  skillFrame.Scroll = CreateFrame("ScrollFrame", "TradeSkillListViewScrollFrame", skillFrame, "UIPanelScrollFrameTemplate");
+  skillFrame.Scroll:SetPoint("TOPLEFT", skillFrame, "TOPLEFT", 20, -20);
+  skillFrame.Scroll:SetPoint("RIGHT", skillFrame, "RIGHT", -30, 0);
+  skillFrame.Scroll:SetPoint("BOTTOM", skillFrame.DoneButton, "TOP", 0, 20);
+  
+  -- Create the edit box and insert it into the scroll frame
+  
+  skillFrame.Box = CreateFrame("EditBox", "TradeSkillListEditBox", skillFrame.Scroll);
+  skillFrame.Box:SetWidth(750);
+  skillFrame.Box:SetHeight(85);
+  skillFrame.Box:SetFontObject("ChatFontNormal");
+  skillFrame.Box:SetMultiLine(true);
+  skillFrame.Scroll:SetScrollChild(skillFrame.Box);
+--  skillFrame.Box:SetText(topText .. text);
+  skillFrame.Box:SetText(itemList);
+
+  skillFrame.Box:HighlightText();  
+  
+  skillFrame.Box:SetScript("OnTextChanged", function(this)
+    skillFrame.Scroll:UpdateScrollChildRect();
+  end);
+  
+  skillFrame:Show();
+  skillFrame.Box:HighlightText();  
+end
+
+function TradeSkillList_OnLoad()
+  SLASH_TRADESKILL1 = TRADESKILL_SLASHCMD_1;
+  SLASH_TRADESKILL2 = TRADESKILL_SLASHCMD_2;
+  SlashCmdList["TRADESKILL"] = StartBuildingSkillList;
+end
---
+++ localization.lua
@@ -0,0 +1,44 @@
+--
+-- English Localization for TradeSkillList
+--
+
+-- Slash command strings
+
+TRADESKILL_SLASHCMD_1       = "/tradeskilllist";
+TRADESKILL_SLASHCMD_2       = "/tsl";
+
+-- For parsing trade skill list results
+
+TRADESKILL_HEADER_TYPE      = "header";
+
+-- Status messages
+
+TRADESKILL_STARTING         = "TradeSkillList: Starting scan.";
+TRADESKILL_NONE_OPEN        = "TradeSkillList: Error - no trade skill window open.";
+TRADESKILL_DONE             = "TradeSkillList: List generated.";
+
+-- The name returned when no trade skill window is open
+
+TRADESKILL_UNKNOWN          = "UNKNOWN";
+
+-- Button text
+
+TRADESKILL_CLOSE            = "Close";
+
+-- Attribution for bottom of generated HTML
+
+TRADESKILL_ATTRIB           = "This listing generated automatically by <a href='http://www.syndicomm.com/wowstuff/tsl/'>TradeSkillList</a> by Eric Shepherd.";
+
+-- Item rarity levels
+
+TRADESKILL_ITEM_UNCOMMON    = "Uncommon";
+TRADESKILL_ITEM_RARE        = "Rare";
+TRADESKILL_ITEM_EPIC        = "Epic";
+
+-- Skill names
+
+TRADESKILL_ENCHANTING       = "Enchanting";
+
+-- Identifies enchants that don't produce items
+
+TRADESKILL_ENCHANT          = "Enchant";
